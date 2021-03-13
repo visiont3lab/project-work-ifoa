2ed011d31026800f22cf4f34d9ae0020a998c625
Manuel <visiont3lab@gmail.com>
2021-03-13 14:46:43 +0100

    solotion

---
+++ app_final.py
@@ -0,0 +1,146 @@
+import streamlit as st
+import utils
+
+# Informazioni Utili
+# pip install streamlit --upgrade
+# Run: streamlit run app.py
+
+@st.cache
+def get_data():
+    # Esegue la funzione solo la priva volta che viene vista
+    utils.collect_data()
+
+# Collect data
+get_data()
+
+# Title
+st.title("Analisi Covid 2020-2021")
+
+# Descrizione Applicazione
+description='''
+## Descrizione
+
+A un anno dall'inizio del lockdown in italia sono stati raccolti numerosi dati relativi
+alla pandemia. In particolare la protezione civile ha creato un [dataset](https://github.com/pcm-dpc/COVID-19)
+che giornalmente ci fornisce informazioni sull'andamento del Covid-19 a livello provinciale,regionale e nazionale.
+L'obbiettivo di questo progetto e di utilizzare questi dati per creare un modello capace di predirre il colore
+di rischio associato a una regione ( bianco, giallo, arancione, arancione scuro, rosso). 
+
+Per poter realizzare il modello (classificatore) sarà necessario:
+
+1. Creare un dataset utilizzando i dati [regionali](https://github.com/pcm-dpc/COVID-19/tree/master/dati-regioni). 
+Questi contengono informazioni relative a numero  di deceduti, ricoverati e tamponi. Vogliamo utilizzare queste informazioni poichè osservando
+i [21 Indicatori forniti dal Ministero della Salute](http://www.salute.gov.it/imgs/C_17_notizie_5152_1_file.pdf)
+tali informaziono sono utilizzati per definire il colore della regione.
+
+2. Ottenere le informazioni associate al colore delle regioni nel tempo. Come è cambiato il colore delle regioni nel tempo. 
+Poichè affrontiamo un problema di supervised learning  è necessario allenare il nostro classificatore partendo da un set di input-ouput data.
+Andremo pertanto a visualizzare e collezionare i dati relativi al colore delle regioni italiane utilizzando i dati delle [aree nuove](https://github.com/pcm-dpc/COVID-19/tree/master/aree/geojson) forniti
+sempre dalla protezione civile.
+
+3. Una volta creato il dataset contenente input-output data andremo ad alleare un classicatore capace di stimare
+il colore della regione.
+
+Per riassumere al fine di completare il progetto sarà necesario:
+
+1. Creare gli input
+2. Creare gli output
+3. Creare il modello
+
+'''
+st.markdown(description)
+
+# --------------------------------------------------------------------
+# Situazione italiana ad oggi italiana
+st.markdown('## Situazione Italiana')
+date = utils.get_date()
+col1, col2, col3= st.beta_columns(3)
+with col1:
+    data_inizio = st.selectbox("Data Inzio", date[1:])
+with col2:
+    data_fine = st.selectbox("Data Fine", date)
+with col3:
+    nomi_regioni = utils.get_nomi_regioni() 
+    nomi_regioni.insert(0,"Italia") 
+    regione = st.selectbox("Analisi", nomi_regioni)
+st.table(utils.get_stats(regione,data_inizio,data_fine)) # try also st.write(), st.dataframe
+options = st.multiselect("Seleziona", utils.get_options(),default=utils.get_options())
+fig_stats = utils.fig_stats(regione,data_inizio,data_fine,options)
+fig_stats_variation = utils.fig_stats_variation(regione,data_inizio,data_fine,options)
+
+st.plotly_chart(fig_stats, use_container_width=True)
+st.plotly_chart(fig_stats_variation, use_container_width=True)
+
+st.markdown('''
+
+TASK:
+
+*  Creare un grafico che mostra la variazione dei deceduti, totale casi,
+dimessi guariti, variazione totale positivi nel range di date scelto. Rispondiamo 
+alla domanda da ..  a .. quanti deceduti ci sono?
+
+''')
+# --------------------------------------------------------------------
+
+# --------------------------------------------------------------------
+# Situazione Colori Regioni
+# https://github.com/pcm-dpc/COVID-19/issues/1045
+st.markdown('''## Situazione Zone di Rischio (Colori ) Regionale ''')
+
+
+regione = st.selectbox("Selziona la Regione", nomi_regioni)
+st.dataframe(utils.get_zone_table(regione))
+
+st.markdown('''
+
+TASK:
+
+* Creare script automatico di aggiornameto colore regioni. Serve a  raccogliere i dati del colore delle regioni.
+* Visualizzare attraverso una mappa il cambiamento di colore delle diverse regioni italiane. Oppure sempre usanda da .. a .. 
+in base alla data visualizzare la mappa delle regione colorata a zone.
+
+Il colore delle regione ad oggi è visibile a nel sito del ministero della satute [Classificazione Regioni e Province autonome
+aggiornamento all'8 marzo](http://www.salute.gov.it/portale/nuovocoronavirus/dettaglioContenutiNuovoCoronavirus.jsp?area=nuovoCoronavirus&id=5351&lingua=italiano&menu=vuoto)
+''')
+# --------------------------------------------------------------------
+
+# --------------------------------------------------------------------
+# Definizione degli input
+st.markdown('''## Definizione degli input per stimare le zone di rischio ''')
+
+st.markdown('''
+
+TODO:
+
+* [Leonardo] Definire gli input che saranno utilizzati per il classificatore bassandosi sul dataset regioni.
+* [Serena] Definire l'indice Rt.
+''')
+# --------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------
+# Testare/allenare il modello
+st.markdown('''## Definizione degli input per stimare le zone di rischio ''')
+
+st.markdown('''
+
+TODO:
+
+* Creare un form  dove verrano inseriti i gli inputs necessari al modello.
+* Allenare e Testare il classificatore.
+''')
+# --------------------------------------------------------------------
+
+
+# --------- Material Extra
+st.markdown('''
+## Link Utili
+
+* [Dati covid italia](https://dati-covid.italia.it/)
+* [Protezione Civilie Covid-19 Open data](https://github.com/pcm-dpc/COVID-19)
+* [21 Indicatori Covid Ministero della Salute](http://www.salute.gov.it/imgs/C_17_notizie_5152_1_file.pdf)
+* [Dataset regioni colore](https://github.com/imcatta/restrizioni_regionali_covid)
+* [Streamlit Api](https://docs.streamlit.io/en/stable/api.html)
+* [Visualizzare la diffussione del covid](https://medium.com/polimi-data-scientists/how-to-visualize-the-spread-of-covid-19-in-italy-6d9ddea18a02)
+* [Come leggere i dati aree (Issue)](https://github.com/pcm-dpc/COVID-19/issues/1045)
+''')
--- utils.py
+++ utils.py
@@ -4,36 +4,114 @@ import base64
 import pandas as pd
 import plotly.graph_objects as go
 from datetime import datetime, time, timedelta, date
+import wget
+from zipfile import ZipFile
+import os
+import json
 
 # pip install streamlit --upgrade
 # pip install streamlit==0.78.0
 
-def fig_stats(regione, data_inizio, data_fine):
-    select = ["deceduti","totale_casi","dimessi_guariti","variazione_totale_positivi"]
+def fig_stats_variation(regione, data_inizio, data_fine,options):
+    #select = ["deceduti","totale_casi","dimessi_guariti","terapia_intensiva","tamponi","isolamento_domiciliare"]
     df = None
+    title = "Variazione Giornaliera"
     if regione=="Italia":
         df = get_data_nazione()
+        df = df[ (df["data"]>=data_inizio) & (df["data"]<=data_fine) ]  
     else:
         df = get_data_regioni()
+        df = df[ (df["data"]>=data_inizio) & (df["data"]<=data_fine) ]
         df = df[df["denominazione_regione"]==regione]
+    
+    # Script to aggregate data 
+    # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.resample.html
+    dft = df.copy()
+    dft = dft.set_index("data")
+    dft["count"] = [1 for i in range(0,len(df))]
+    agg = {"count" : "size"}
+    for s in options:
+        agg[s] = "median"
+    dft = dft.resample('1D').agg(agg)
+
+    # Variation daily
+    df = {"data": dft.index[1:]}
+    for s in options:
+        start = dft[s][:-1].values
+        end = dft[s][1:].values
+        df[s] = ( end - start ) 
+        #df[s] =  np.round( ( end / start -1 )*100,2)
+        
+    df = pd.DataFrame(df)
+    df = df.set_index("data")
+    #dft.dropna()
+    #print(dft.head())
+    # Rolling average variation
+
 
     #df = df[ (df["data"]>=data_inizio) & (df["data"]<=data_fine) ]
     fig = go.Figure()
-    for name in select:
-        fig.add_trace(go.Scatter(x=df["data"], y=df[name],
-                      mode='lines',#mode='lines+markers',
+    for name in options:
+        fig.add_trace(go.Scatter(x=df.index, y=df[name],
+                      mode='lines+markers',#mode='lines+markers',
                       name=name.replace("_"," "),
                       hoverlabel_namelength=-1))
     fig.update_layout(
         showlegend=True,
         hovermode = "x",
+        yaxis_title = "Persone",
         #paper_bgcolor = "rgb(0,0,0)" ,
         #plot_bgcolor = "rgb(10,10,10)" , 
         legend=dict(orientation="h",yanchor="bottom", y=1.02,xanchor="right", x=1,title_text=""),
         dragmode="pan",
         title=dict(
             x = 0.5,
-            #text = title,
+            y = 0.05,
+            text = title,
+            font=dict(
+                size = 20,
+                color = "rgb(0,0,0)"
+            )
+        )
+    )
+    return fig
+
+def fig_stats(regione, data_inizio, data_fine,options):
+    #select = ["deceduti","totale_casi","dimessi_guariti","terapia_intensiva","tamponi","isolamento_domiciliare"]
+    df = None
+    title = "Andamento Cumulativo"
+    if regione=="Italia":
+
+        df = get_data_nazione()
+        df = df[ (df["data"]>=data_inizio) & (df["data"]<=data_fine) ]  
+        df = df.set_index("data")
+    
+    else:
+        df = get_data_regioni()
+        df = df[ (df["data"]>=data_inizio) & (df["data"]<=data_fine) ]
+        df = df[df["denominazione_regione"]==regione]
+        df = df.set_index("data")
+    
+
+    #df = df[ (df["data"]>=data_inizio) & (df["data"]<=data_fine) ]
+    fig = go.Figure()
+    for name in options:
+        fig.add_trace(go.Scatter(x=df.index, y=df[name],
+                      mode='lines+markers',#mode='lines+markers',
+                      name=name.replace("_"," "),
+                      hoverlabel_namelength=-1))
+    fig.update_layout(
+        showlegend=True,
+        hovermode = "x",
+        yaxis_title = "Persone",
+        #paper_bgcolor = "rgb(0,0,0)" ,
+        #plot_bgcolor = "rgb(10,10,10)" , 
+        legend=dict(orientation="h",yanchor="bottom", y=1.02,xanchor="right", x=1,title_text=""),
+        dragmode="pan",
+        title=dict(
+            x = 0.5,
+            y = 0.05,
+            text = title,
             font=dict(
                 size = 20,
                 color = "rgb(0,0,0)"
@@ -64,6 +142,10 @@ def get_nomi_regioni():
     df = get_data_regioni()
     #df["data"] = [ datetime.strptime(d, "%Y-%m-%dT%H:%M:%S") for d in  df["data"]]
     return df["denominazione_regione"].unique().tolist()
+
+def get_options():
+    select = ["deceduti","totale_casi","dimessi_guariti","terapia_intensiva","tamponi","isolamento_domiciliare"]
+    return select
 
 def get_date():
     df = get_data_nazione()
@@ -178,4 +260,98 @@ def get_data_locally():
     df = pd.read_csv(url)
     df.to_csv("data/dpc-covid19-ita-andamento-nazionale.csv")
 
-#get_data_locally()
+
+def get_zone_table(regione):
+    #df = pd.read_csv("data/dpc-covid-19-aree.csv")
+    df = pd.read_csv("data/dpc-covid19-ita-regioni-zone.csv")
+    df["data"] = [ datetime.strptime(d, "%Y-%m-%d %H:%M:%S").date() for d in  df["data"]]
+    df = df.sort_values(by=["data"],ascending=False)
+    if regione!="Italia":
+        df = df[df["denominazione_regione"]==regione]
+    inputs = ["data","denominazione_regione","zona"] #"ricoverati_con_sintomi","terapia_intensiva","totale_ospedalizzati","totale_positivi","isolamento_domiciliare","deceduti","dimessi_guariti","tamponi","zona"]
+    df = df[inputs]
+    return df
+
+def collect_data():
+
+    url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
+    df = pd.read_csv(url)
+    df.to_csv("data/dpc-covid19-ita-andamento-nazionale.csv")
+
+    url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
+    df = pd.read_csv(url)
+    df.to_csv("data/dpc-covid19-ita-regioni.csv")
+
+    url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv"
+    df = pd.read_csv(url)
+    df.to_csv("data/dpc-covid19-ita-province.csv")
+
+    '''
+    # bash
+    wget https://github.com/pcm-dpc/COVID-19/raw/master/aree/geojson/dpc-covid-19-aree-nuove-g-json.zip
+    unzip dpc-covid-19-aree-nuove-g-json.zip
+    '''
+    url = 'https://github.com/pcm-dpc/COVID-19/raw/master/aree/geojson/dpc-covid-19-aree-nuove-g-json.zip'
+    filenameZip = wget.download(url,out="data/dpc-covid-19-aree-nuove-g-json.zip")
+    with ZipFile(filenameZip, 'r') as zipObj:
+        zipObj.extractall("data/")
+        print('File is unzipped') 
+        zipObj.close()
+    os.remove(filenameZip)
+
+    # Process json file to retrieve data
+    with open('data/dpc-covid-19-aree-nuove-g.json') as f:
+        data = json.load(f)
+        data_dict = {
+            "regione" : [],
+            "data_inizio": [],
+            "data_fine": [],
+            "colore" : [],
+            "link": []
+        }
+        color_dict = {"art.1": "gialla","art.2": "arancione","art.3": "rossa", "art.1 comma 11" : "bianca" }
+        lista_regioni = []
+        for d in data["features"]:
+            p = d["properties"]
+            data_inizio = datetime.strptime(p["datasetIni"], "%d/%m/%Y")
+
+            if p["datasetFin"]==" ":
+                data_fine = datetime.now() #.date()
+            else:
+                data_fine = datetime.strptime(p["datasetFin"], "%d/%m/%Y") #.date()
+            
+            regione = p["nomeTesto"]
+            colore = color_dict[ p["legSpecRif"] ]
+
+            #if (data_inizio not in data_dict["data_inizio"]):
+            data_dict["regione"].append(regione)
+            data_dict["data_inizio"].append(data_inizio)
+            data_dict["data_fine"].append(data_fine)
+            data_dict["colore"].append(colore)
+            data_dict["link"].append(p["legLink"])
+
+        df = pd.DataFrame(data_dict)
+        df.to_csv("data/dpc-covid-19-aree.csv")
+
+        # Update dataset Regioni
+        df_r = pd.read_csv("data/dpc-covid19-ita-regioni.csv")
+        df_r["data"] = [ datetime.strptime(d, "%Y-%m-%dT%H:%M:%S") for d in  df_r["data"]]
+        colors = []
+        data_inizio_zone = datetime(2020,11,6)
+        # Introdotto da novembre concettto di zona
+        for index, row in df_r.iterrows():
+            #print(data,regione)
+            if row["data"]>=data_inizio_zone:
+                dff = df[df["regione"]==row["denominazione_regione"]]
+                #print(row["data"])
+                dff = dff[ (  (row["data"]>=dff["data_inizio"]) & (row["data"] <=dff["data_fine"] ) )]
+                if dff.empty:
+                    colors.append("bianca")
+                else:
+                    colors.append(dff["colore"].tolist()[0])
+            else:
+                colors.append("unknown")
+
+        df_r["zona"] = colors   
+        df_r.to_csv("data/dpc-covid19-ita-regioni-zone.csv")
+        
