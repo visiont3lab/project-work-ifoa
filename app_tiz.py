05a25bf2e4c9fa03b4540349a4a18584975c139e
Tiziana <joker.tix@gmail.com>
2021-03-28 19:56:20 +0200

    data selector fixed

--- app_tiz.py
+++ app_tiz.py
@@ -12,24 +12,31 @@ date = [datetime.strptime(d, "%Y-%m-%d %H:%M:%S.%f") for d in  df["data_inizio"]
 df["data"] = date
 color_discrete_map = {'unknown':  'rgb(1,0,0)', 'bianca': 'white', 'gialla': 'yellow', 'arancione': 'orange','rossa': 'red'}
 
-data_selection = st.date_input('Seleziona una data:')
+input_date = st.date_input('Seleziona una data:')
+selected_date = input_date.strftime("%Y-%m-%d %H:%M:%S.%f")
+map_df = df[df["data"]==selected_date]
 
-map_df = df[df["data"]==data_selection]
-fig = px.choropleth_mapbox(data_frame=map_df, 
+start_date = "2020-11-22 00:00:00.0" 
+
+if selected_date < start_date:
+    st.error('Non sono presenti dati di zona per la data inserita. Inserire un\'altra data.')
+else:
+    fig = px.choropleth_mapbox(data_frame=map_df, 
                 locations='regione',                    # dataframe column to match with featureidkey
                 geojson=italy_regions_geo,              # geojson
                 featureidkey='properties.reg_name',     # path of the geojson field to match with locations
-                color="zona",
+                color="zona",                           # colonna df a cui fare l'assegnazione colore
                 color_discrete_map=color_discrete_map,  # color map defined above
                 center = {"lat": 41.9, "lon": 12.5},    # map centering
                 zoom=4.5,                               # map zooming
                 opacity=0.5,                            # color opacity
-                title='Italian Rt zones',
+                title='Zone colore dell\'Italia',
                 mapbox_style="carto-positron",
                 height=600,                    
-)
+    )
 
-fig.update_geos(showcountries=False, showcoastlines=False, showland=False, fitbounds="locations")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_geos(showcountries=False, showcoastlines=False, showland=False, fitbounds="locations")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-st.plotly_chart(fig)
+    st.plotly_chart(fig)
+     
